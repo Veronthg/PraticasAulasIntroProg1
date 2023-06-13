0d021ed547bc9a4091a3801d11c4269364978040
veron <veronthiago21@gmail.com>
2023-06-12 23:08:49 -0300

    Quarta questão

---
+++ Exercícios_1_4_Celsius_para_Farenheit.py
@@ -0,0 +1,8 @@
+#Definição das variáveis a serem utilizadas - Entrada
+C = float(input("Informe a temperatura atual em Celsius a ser convertida: "))
+
+#Procedimento
+F = (C * 9 / 5) + 32
+
+#Saída
+print("A temperatura atual convertida de Celsius para Farenheit é: ",F)
