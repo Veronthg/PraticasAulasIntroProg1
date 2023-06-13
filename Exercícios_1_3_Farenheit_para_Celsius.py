e65387f5506400066fe19e45961f0af5423c67d2
veron <veronthiago21@gmail.com>
2023-06-12 23:06:20 -0300

    Terceira questão

--- Exercícios_1_3_Farenheit_para_Celsius.py
+++ Exercícios_1_3_Farenheit_para_Celsius.py
@@ -1,8 +1,6 @@
 #Definição das variáveis a serem utilizadas - Entrada
 F = float(input("Informe a temperatura em Farenheit: "))
-
 #Procedimento
 C = (5 * (F - 32) / 9)
-
 #Saída
 print("A temperatura atual convertida de Farenheit para Celsius é: ", C)
