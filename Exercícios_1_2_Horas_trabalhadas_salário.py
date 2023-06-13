12b16ea0e95e38d6a344a06e8b989fe0b39868ef
veron <veronthiago21@gmail.com>
2023-06-12 22:49:27 -0300

    Segunda Questão

---
+++ Exercícios_1_2_Horas_trabalhadas_salário.py
@@ -0,0 +1,7 @@
+#Definição das variáveis a serem utilizadas - Entrada
+salariohora = float(input("Informe quanto você recebe por hora trabalhada: "))
+horastrab = float(input("Informe quantas horas você trabalhou: "))
+#Procedimento
+salariototal = salariohora * horastrab
+#Saída
+print("O seu salário total é: R$",salariototal)
